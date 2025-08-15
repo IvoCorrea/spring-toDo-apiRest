262dfcb7e2f2395ccdf144f7d0fda3103e7162d5
IvoCorrea <ivoaurelio30@gmail.com>
2025-08-14 22:23:44 -0300

    feat: initialize Spring Boot project with base structure

---
+++ src/main/java/com/todo/desafio_chatgpt/DesafioChatgptApplication.java
@@ -0,0 +1,13 @@
+package com.todo.desafio_chatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioChatgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioChatgptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todo/desafio_chatgpt/DesafioChatgptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.desafio_chatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioChatgptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
